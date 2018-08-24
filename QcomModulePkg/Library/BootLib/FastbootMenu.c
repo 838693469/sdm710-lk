ef9ad760472273ba9f9c378bc407e319b5c01aef
Kiran Raparthy <kraparth@codeaurora.org>
2018-08-25 00:35:48 +0530

    Revert "RUMI TALOS: Disable VB for RUMI"
    This reverts commit 558843dff3ecf99448b3b9877ab8e42ba9b6fc3b.
    
    Change-Id: I794b96586905461662ea6d064466fea1ad336156

--- QcomModulePkg/Library/BootLib/FastbootMenu.c
+++ QcomModulePkg/Library/BootLib/FastbootMenu.c
@@ -323,8 +323,6 @@ VOID DisplayFastbootMenu (VOID)
   OPTION_MENU_INFO *OptionMenuInfo;
   OptionMenuInfo = &gMenuInfo;
 
-DEBUG ((EFI_D_INFO, "[%p]\n", OptionMenuInfo));
-
   if (FixedPcdGetBool (EnableDisplayMenu)) {
     DrawMenuInit ();
     OptionMenuInfo->LastMenuType = OptionMenuInfo->Info.MenuType;
